bf29b536217af45ea2d38f802fb413533f69a7fc
FriendHOAUF <140300214+FriendHOAUF@users.noreply.github.com>
2025-07-30 18:56:45 -0700

    Update main.py

--- Pong_The_Game/main.py
+++ Pong_The_Game/main.py
@@ -1,1 +1,17 @@
 import pygame
+
+pygame.init()
+
+# Game windown
+screen = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption("Pygame Test")
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+# Quit Pygame
+pygame.quit()
